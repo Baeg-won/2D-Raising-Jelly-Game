56b34ab696f81c342f215290ab931735ecb20456
Daegwon Kim <kdg1449@naver.com>
2022-03-22 20:13:29 +0900

    젤리 키우기 3일차
    젤리 판매 기능 추가 및 일부 코드 수정

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,12 +8,51 @@ public class GameManager : MonoBehaviour
     public int jelatin;
     public int gold;
 
+    public int max_jelatin;
+    public int max_gold;
+
+    public int[] jelly_goldlist;
+    public bool isSell;
+
+    public RuntimeAnimatorController[] level_ac;
+
     public Text jelatin_text;
     public Text gold_text;
+
+    void Awake()
+    {
+        isSell = false;
+    }
 
     void LateUpdate()
     {
         jelatin_text.text = string.Format("{0:n0}", Mathf.SmoothStep(float.Parse(jelatin_text.text), jelatin, 0.5f));
         gold_text.text = string.Format("{0:n0}", Mathf.SmoothStep(float.Parse(gold_text.text), gold, 0.5f));
+    }
+
+    public void ChangeAc(Animator anim, int level)
+    {
+        anim.runtimeAnimatorController = level_ac[level - 1];
+    }
+
+    public void GetJelatin(int id, int level)
+    {
+        jelatin += (id + 1) * level;
+
+        if (jelatin > max_jelatin)
+            jelatin = max_jelatin;
+    }
+
+    public void GetGold(int id, int level)
+    {
+        gold += jelly_goldlist[id] * level;
+
+        if (gold > max_gold)
+            gold = max_gold;
+    }
+
+    public void CheckSell()
+    {
+        isSell = isSell == false;
     }
 }
--- Assets/Scripts/Jelly.cs
+++ Assets/Scripts/Jelly.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Jelly : MonoBehaviour
 {
-    public int move_delay;
-    public int move_time;
+    public int id;
+    public int level;
+    public float exp;
+    public float required_exp;
+    public float max_exp;
+
+    public GameManager game_manager;
+    public GameObject left_top;
+    public GameObject right_bottom;
+
+    float pick_time;
+
+    int move_delay;
+    int move_time;
 
     float speed_x;
     float speed_y;
+
     bool isWandering;
     bool isWalking;
 
@@ -24,20 +38,74 @@ public class Jelly : MonoBehaviour
         isWalking = false;
     }
 
+    void Update()
+    {
+        if(exp < max_exp)
+            exp += Time.deltaTime;
+
+        if (exp > required_exp * level && level < 3)
+            game_manager.ChangeAc(anim, ++level);
+    }
+
     void FixedUpdate()
     {
         if (!isWandering)
             StartCoroutine(Wander());
         if (isWalking)
             Move();
+
+        float pos_x = transform.position.x;
+        float pos_y = transform.position.y;
+
+        if (pos_x < left_top.transform.position.x || pos_x > right_bottom.transform.position.x)
+            speed_x = -speed_x;
+        if (pos_y > left_top.transform.position.y || pos_y < right_bottom.transform.position.y)
+            speed_y = -speed_y;
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnMouseDown()
     {
-        if (collision.gameObject.name.Contains("Bottom") || collision.gameObject.name.Contains("Top"))
-            speed_y = -speed_y;
-        else if (collision.gameObject.name.Contains("Left") || collision.gameObject.name.Contains("Right"))
-            speed_x = -speed_x;
+        isWalking = false;
+        anim.SetBool("isWalk", false);
+        anim.SetTrigger("doTouch");
+
+        if(exp < max_exp) ++exp;
+
+        game_manager.GetJelatin(id, level);
+    }
+
+    void OnMouseDrag()
+    {
+        pick_time += Time.deltaTime;
+
+        if (pick_time < 0.1f) return;
+
+        isWalking = false;
+        anim.SetBool("isWalk", false);
+        anim.SetTrigger("doTouch");
+
+        Vector3 mouse_pos = Input.mousePosition;
+        Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(mouse_pos.x, mouse_pos.y, mouse_pos.y));
+
+        transform.position = point;
+    }
+
+    void OnMouseUp()
+    {
+        pick_time = 0;
+
+        if (game_manager.isSell) {
+            game_manager.GetGold(id, level);
+
+            Destroy(gameObject);
+        }
+
+        float pos_x = transform.position.x;
+        float pos_y = transform.position.y;
+
+        if (pos_x < left_top.transform.position.x || pos_x > right_bottom.transform.position.x ||
+            pos_y > left_top.transform.position.y || pos_y < right_bottom.transform.position.y)
+            transform.position = new Vector3(0, -1, 0);
     }
 
     void Move()
